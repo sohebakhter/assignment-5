a6bc4bf1232f9ba6bbc6a1ed7b71de182b972ae5
sohebakhter <sohebakhterbadhan@gmailcom>
2025-08-27 16:04:01 +0600

    added call history button dynamically

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+callHistoryData = [];
+
 
 // nav heart count section
 const buttons = document.getElementsByClassName('btn-heart')
@@ -13,6 +15,8 @@ for (button of buttons) {
 
 }
 
+
+
 // call buttons section
 
 
@@ -25,15 +29,50 @@ for (const callButton of callButtons) {
     callButton.addEventListener('click', function () {
 
         alert('Calling... ' + document.getElementById('service-name').innerText + ' ' + document.getElementById('service-number').innerText);
-        
+
         coinCount -= 20;
-        
-        if(coinCount !== 20 && coinCount < 0){
+
+        if (coinCount !== 20 && coinCount < 0) {
             alert('Not Enough Coin Please Recharge');
             return;
         }
-        
+
         coin.textContent = coinCount;
+
+        const data = {
+            name: document.getElementById('service-name').innerText,
+            number: document.getElementById('service-number').innerText,
+            time: new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+        console.log(callHistoryData);
+        
+        
+        const callHistoryContainer = document.getElementById('history-card-container')
+        callHistoryContainer.innerText = ''
+        
+        for (const data of callHistoryData) {
+            const div = document.createElement('div')
+            div.innerHTML = `
+                <div class="history-card flex justify-between items-center bg-[#fafafa] p-3 rounded-xl shadow-md mt-3">
+                                    <div>
+                                        <h3 class="text-xl font-semibold">
+                                            ${data.name}
+                                        </h3>
+                                        <p class="text-lg">
+                                            ${data.number}
+                                        </p>
+                                    </div>
+                                    <p class="text-lg">${data.time}</p>
+                                </div>
+            `
+            callHistoryContainer.appendChild(div);
+        
+        }
         
     });
+    
 }
+
+
+
